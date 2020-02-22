3891d464e0392a069a42810043e044efe0a5488c
Eva <eva.zhuk@gmail.com>
2020-02-22 22:53:41 +0200

    Links (#6)
    * Links component added
    
    * Added links query
    
    * Links typography changed
    
    * Refactor

---
+++ src/components/homepage/Links.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export type LinksBlock = LinkType[]
+
+export type LinkType = {
+    text: string,
+    url: string
+}
+
+export type Props = {
+    linksAttr: string
+}
+
+const Link: React.FC<LinkType> = (link) => {
+    return (
+        <a href={link.url} className="is-uppercase has-text-weight-semibold">{link.text}</a>
+    )
+}
+
+const Links: React.FC<Props> = ({ linksAttr }) => {
+    const linksBlock = JSON.parse(decodeURI(linksAttr)) as LinksBlock | undefined
+
+    if (linksBlock === undefined) return null
+
+    return (
+        <>
+            <div className="columns">
+                <div className="column">
+                    <Link {...linksBlock[0]}/>
+                </div>
+                <div className="column">
+                    <Link {...linksBlock[1]} />
+                </div>
+                <div className="column">
+                    <Link {...linksBlock[2]} />
+                </div>
+            </div>
+            <div className="columns">
+                <div className="column">
+                    <Link {...linksBlock[3]} />
+                </div>
+                <div className="column">
+                    <Link {...linksBlock[4]} />
+                </div>
+                <div className="column">
+                    <Link {...linksBlock[5]} />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Links
