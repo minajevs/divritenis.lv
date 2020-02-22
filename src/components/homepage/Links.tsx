a3a898b4951b5fbf9a046ba93c18e4438d910810
Eva <eva.zhuk@gmail.com>
2020-02-22 21:15:19 +0200

    Links typography changed

--- src/components/homepage/Links.tsx
+++ src/components/homepage/Links.tsx
@@ -1,35 +1,49 @@
 import React from 'react'
 
-export type Link = {
+export type LinkType = {
     title: string,
     url: string
 }
 
+const Link: React.FC<LinkType> = (link) => {
+    return (
+        <a href={link.url} className="is-uppercase has-text-weight-semibold">{link.title}</a>
+    )
+}
+
 const Links: React.FC = () => {
+    const links: LinkType[] = [
+        { title: 'test', url: 'url' },
+        { title: 'test', url: 'url' },
+        { title: 'test', url: 'url' },
+        { title: 'test', url: 'url' },
+        { title: 'test', url: 'url' },
+        { title: 'test', url: 'url' },
+    ]
     return (
         <>
-        <div className="columns">
-            <div className="column">
-                Zinas
+            <div className="columns">
+                <div className="column">
+                    <Link title={links[0].title} url={links[0].url} />
+                </div>
+                <div className="column">
+                    <Link title={links[1].title} url={links[1].url} />
+                </div>
+                <div className="column">
+                    <Link title={links[2].title} url={links[2].url} />
+                </div>
             </div>
-            <div className="column">
-                Noderigi
+            <div className="columns">
+                <div className="column">
+                    <Link title={links[3].title} url={links[3].url} />
+                </div>
+                <div className="column">
+                    <Link title={links[4].title} url={links[4].url} />
+                </div>
+                <div className="column">
+                    <Link title={links[5].title} url={links[5].url} />
+                </div>
             </div>
-            <div className="column">
-                Dokumenti
-            </div>
-        </div>
-        <div className="columns">
-            <div className="column">
-                Kalendars
-                 </div>
-            <div className="column">
-                Drosiba
-            </div>
-            <div className="column">
-                Apvieniba
-            </div>
-        </div>
         </>
     )
 }
