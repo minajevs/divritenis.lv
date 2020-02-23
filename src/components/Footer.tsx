d3507bf8ae3da02c825f29b69c9a163bcb61a99d
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2020-02-23 19:24:39 +0200

    Added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export type Props = {}
+
+export const Footer: React.FC<Props> = ({ }) => {
+    return (
+        <footer className="footer">
+            <div className="content has-text-centered">
+                <p>
+                    Info par lapu
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
