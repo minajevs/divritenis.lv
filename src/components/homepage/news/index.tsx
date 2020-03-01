a3617d875a02edb7eb6eea8f0a8c3ca9ce0db4ec
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2020-03-01 16:01:26 +0200

    StaticQuery

--- src/components/homepage/news/index.tsx
+++ src/components/homepage/news/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import Card from './Card'
 
@@ -32,33 +32,26 @@ const query = graphql`
 export type Props = {}
 
 export const News: React.FC<Props> = ({ }) => {
-  return (
-    <StaticQuery
-      query={query}
-      render={(data: LatestNewsQuery) => {
-        const { edges } = data.allWordpressPost
+  const data = useStaticQuery<LatestNewsQuery>(query)
+  const { edges } = data.allWordpressPost
 
-        return (
-          <div className="news-body">
-            <div className="container">
-              <div className="columns is-multiline">
-                {edges.map(({ node }, i) => (
-                  <div className="column is-one-third" key={i}>
-                    <Card
-                      title={node.title ?? ""}
-                      excerpt={node.acf?.excerpt ?? ""}
-                      image={{ url: node.acf?.preview_image?.source_url ?? "", alt: node.acf?.preview_image?.alt_text ?? "" }}
-                      path={node.path ?? ""}
-                    />
-                  </div>
-                ))}
-              </div>
+  return (
+    <div className="news-body">
+      <div className="container">
+        <div className="columns is-multiline">
+          {edges.map(({ node }, i) => (
+            <div className="column is-one-third" key={i}>
+              <Card
+                title={node.title ?? ""}
+                excerpt={node.acf?.excerpt ?? ""}
+                image={{ url: node.acf?.preview_image?.source_url ?? "", alt: node.acf?.preview_image?.alt_text ?? "" }}
+                path={node.path ?? ""}
+              />
             </div>
-          </div>)
-      }
-      }
-    />
-  )
+          ))}
+        </div>
+      </div>
+    </div>)
 }
 
 export default News
