14db8752a1f4970d18890cf8e15f78ebbf112610
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2020-07-04 13:53:47 +0300

    Started the ball rolling

---
+++ src/components/PostInfo.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export type Props = {
+
+}
+
+export const PostInfo: React.FC<Props> = ({ }) => {
+    return (
+        <div>
+            <div className="box">
+                Hello world
+            </div>
+        </div>
+    )
+}
+
+export default PostInfo
--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/layout'
+import { BreadcrumbsFactory } from '../components/layout/breadcrumbs'
 
 import { BlogPostByIdQuery, Wordpress__Category, Maybe } from '../../graphql-types'
+import PostInfo from '../components/PostInfo'
 
 type TemplateProps = {
   data: BlogPostByIdQuery
@@ -19,50 +20,51 @@ export const BlogPostTemplate: React.FC<TemplateProps> = ({ data }) => {
 
   const { title, content, date, author, categories, tags } = post
   return (
-    <section className="section">
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <div dangerouslySetInnerHTML={{ __html: content }} />
-            <div style={{ marginTop: `4rem` }}>
-              <p>
-                {date} - posted by{' '}
-                <Link to={`/author/${author?.slug}`}>{author?.name}</Link>
-              </p>
-              {categories && categories.length ? (
-                <div>
-                  <h4>Categories</h4>
-                  <ul className="taglist">
-                    {categories.map(category => (
-                      <li key={`${category?.slug}cat`}>
-                        <Link to={`/categories/${category?.slug}/`}>
-                          {category?.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : null}
-              {tags && tags.length ? (
-                <div>
-                  <h4>Tags</h4>
-                  <ul className="taglist">
-                    {tags.map(tag => (
-                      <li key={`${tag?.slug}tag`}>
-                        <Link to={`/tags/${tag?.slug}/`}>{tag?.name}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : null}
-            </div>
+    <div className="container content">
+      <div className="columns">
+        <div className="column is-three-quarters">
+          <h1 className="is-size-2 has-text-weight-bold is-bold-light">
+            {title}
+          </h1>
+          <div dangerouslySetInnerHTML={{ __html: content }} />
+        </div>
+        <div className="column">
+          <PostInfo />
+          <div style={{ marginTop: `4rem` }}>
+            <p>
+              {date} - posted by{' '}
+              <Link to={`/author/${author?.slug}`}>{author?.name}</Link>
+            </p>
+            {categories && categories.length ? (
+              <div>
+                <h4>Categories</h4>
+                <ul className="taglist">
+                  {categories.map(category => (
+                    <li key={`${category?.slug}cat`}>
+                      <Link to={`/categories/${category?.slug}/`}>
+                        {category?.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+            {tags && tags.length ? (
+              <div>
+                <h4>Tags</h4>
+                <ul className="taglist">
+                  {tags.map(tag => (
+                    <li key={`${tag?.slug}tag`}>
+                      <Link to={`/tags/${tag?.slug}/`}>{tag?.name}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
-    </section>
+    </div>
   )
 }
 
@@ -74,7 +76,7 @@ const BlogPost: React.FC<Props> = ({ data }) => {
   const { wordpressPost: post } = data
 
   return (
-    <Layout>
+    <Layout breadcrumbs={['page', BreadcrumbsFactory.currentPage(post?.title || '')]}>
       <Helmet title={`${post?.title} | Blog`} />
       <BlogPostTemplate data={data} />
     </Layout>
