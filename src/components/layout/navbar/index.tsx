02a6c9e1f6af79836be82e87c7e076d5dee1d8d4
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2020-07-20 20:47:43 +0300

    Add dynamic navbar

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 
 import Logo from './Logo'
 import Links from './Links'
@@ -6,29 +6,40 @@ import SocialLinks from './SocialLinks'
 
 import './navbar.scss'
 
-const Navbar: React.FC = () => (
-  <>
-    <nav role="header" aria-label="header row" className="navbar is-transparent">
-      <div className="container">
-        <div className="navbar-brand">
-          <Logo />
-        </div>
-        <div className="navbar-end">
-          <SocialLinks />
-        </div>
-      </div>
-    </nav>
-
-    <nav role="navigation" aria-label="main navigation" className="navbar is-transparent row">
-      <div className="container">
-        <div className="navbar-menu">
-          <div className="columns is-gapless is-vcentered is-full-width has-text-centered">
-            <Links linkClassName="navbar-item" />
+const Navbar: React.FC = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const toggleMenu = useCallback(() => setMobileMenuOpen(prev => !prev), [setMobileMenuOpen])
+  return (
+    <>
+      <nav role="header" aria-label="header row" className="navbar is-transparent">
+        <div className="container">
+          <div className="navbar-brand">
+            <Logo />
+          </div>
+          <div className="navbar-end">
+            <SocialLinks />
           </div>
         </div>
-      </div>
-    </nav>
-  </>
-)
+      </nav>
+
+      <nav role="navigation" aria-label="main navigation" className="navbar is-transparent row">
+        <div className="container">
+          <div className="navbar-brand">
+            <a role="button" onClick={toggleMenu} className={`navbar-burger burger ${mobileMenuOpen ? "is-active" : ""}`} data-target="navMenu">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+          <div id="navMenu" className={`navbar-menu ${mobileMenuOpen ? "is-active" : ""}`}>
+            <div className="columns is-gapless is-vcentered is-full-width has-text-centered">
+              <Links linkClassName="navbar-item" />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
 
 export default Navbar
